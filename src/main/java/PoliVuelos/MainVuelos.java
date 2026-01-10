66362b8eb540e1f35d5b880b2a44415d983bd167
Kayden335 <karengrefa335@gmail.com>
2026-01-10 13:35:49 -0500

    Primer commit

---
+++ src/main/java/PoliVuelos/MainVuelos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package PoliVuelos;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MainVuelos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
