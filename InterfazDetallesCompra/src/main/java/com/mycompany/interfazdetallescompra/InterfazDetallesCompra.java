eb90fafa17468bbfea982b92f6560719ee401e2b
Ariel3141592 <chicho050820052@gmail.com>
2026-01-18 19:39:06 -0500

    Detalles de la compra

---
+++ InterfazDetallesCompra/src/main/java/com/mycompany/interfazdetallescompra/InterfazDetallesCompra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfazdetallescompra;
+
+/**
+ *
+ * @author chich
+ */
+public class InterfazDetallesCompra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ InterfazDetallesCompra/src/main/java/toggleBtn.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chich
+ */
+class toggleBtn {
+
+    static boolean isSelected() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setText(String si) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
